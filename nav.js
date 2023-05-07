9798a727cdf40ef160429ca97e4148c3c6072d5d
Quinton Ashley <qashto@gmail.com>
2023-05-07 18:07:12 -0500

    1.0.49

--- nav.js
+++ nav.js
@@ -1,18 +1,45 @@
-$('#referenceLinks').hover(
-	() => {
-		$('.refs').show();
-		$('#referenceLink').css('height', 0);
-		$('#referenceLink').css('opacity', 0);
-	},
-	() => {
-		$('.refs').hide();
-		$('#referenceLink').css('height', 'unset');
-		$('#referenceLink').css('opacity', 1);
-	}
-);
+// Get the element with id 'referenceLinks'
+var referenceLinks = document.getElementById('referenceLinks');
 
-$('#referenceLink').on('click', () => {
-	$('.refs').show();
-	$('#referenceLink').css('height', 0);
-	$('#referenceLink').css('opacity', 0);
+// Add event listeners for mouseenter and mouseleave
+referenceLinks.addEventListener('mouseenter', function () {
+	// Show elements with class 'refs'
+	var refs = document.getElementsByClassName('refs');
+	for (var i = 0; i < refs.length; i++) {
+		refs[i].style.display = 'block';
+	}
+
+	// Set height and opacity of element with id 'referenceLink'
+	var referenceLink = document.getElementById('referenceLink');
+	referenceLink.style.height = '0';
+	referenceLink.style.opacity = '0';
+});
+
+referenceLinks.addEventListener('mouseleave', function () {
+	// Hide elements with class 'refs'
+	var refs = document.getElementsByClassName('refs');
+	for (var i = 0; i < refs.length; i++) {
+		refs[i].style.display = 'none';
+	}
+
+	// Set height and opacity of element with id 'referenceLink'
+	var referenceLink = document.getElementById('referenceLink');
+	referenceLink.style.height = 'unset';
+	referenceLink.style.opacity = '1';
+});
+
+// Get the element with id 'referenceLink'
+var referenceLink = document.getElementById('referenceLink');
+
+// Add click event listener
+referenceLink.addEventListener('click', function () {
+	// Show elements with class 'refs'
+	var refs = document.getElementsByClassName('refs');
+	for (var i = 0; i < refs.length; i++) {
+		refs[i].style.display = 'block';
+	}
+
+	// Set height and opacity of element with id 'referenceLink'
+	referenceLink.style.height = '0';
+	referenceLink.style.opacity = '0';
 });
